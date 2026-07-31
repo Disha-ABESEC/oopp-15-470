ff9fc7dcc6e22514b51caba8f855029b57a03e4c
disha garg <dishaa27garg@gmail.com>
2026-07-31 10:27:05 +0530

    pgm5

---
+++ pgm5.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    vector<int> num = {10, 20, 30, 40, 50};
+
+    cout << "Elements of vector are: " << endl;
+
+    for(auto i : num)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
