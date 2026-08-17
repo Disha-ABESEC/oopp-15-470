0f0062c671884dc288aec582b19586c48c8ad791
disha garg <dishaa27garg@gmail.com>
2026-08-17 21:19:05 +0530

    prg4

--- prg4.cpp
+++ prg4.cpp
@@ -1,49 +1,18 @@
+
 #include<iostream>
+#include<vector>
 using namespace std;
-
-
-inline int add(int a, int b)
-{
-    return a + b;
-}
-
-
-int multiply(int a, int b = 2)
-{
-    return a * b;
-}
-
-
-int subtract(int a, int b)
-{
-    return a - b;
-}
-
-float subtract(float a, float b)
-{
-    return a - b;
-}
 
 int main()
 {
-    int x, y;
+    vector<int> num = {10, 20, 30, 40, 50};
 
-    cout << "Enter two numbers: ";
-    cin >> x >> y;
+    cout << "Elements of vector are: " << endl;
 
-    cout << "Addition = " << add(x, y) << endl;
-
-    cout << "Multiplication (default value of b = 2) = "
-         << multiply(x) << endl;
-
-    cout << "Multiplication (user values) = "
-         << multiply(x, y) << endl;
-
-    cout << "Subtraction (int) = "
-         << subtract(x, y) << endl;
-
-    cout << "Subtraction (float) = "
-         << subtract(10.5f, 3.2f) << endl;
+    for(auto i : num)
+    {
+        cout << i << " ";
+    }
 
     return 0;
 }
