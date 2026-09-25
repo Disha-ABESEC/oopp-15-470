0548dd713dafe4d59ee8acfa8caff6a6581d9825
disha garg <dishaa27garg@gmail.com>
2026-09-25 10:53:46 +0530

    ques1

---
+++ lab2/ques1.cpp
@@ -0,0 +1,16 @@
+// Reference Variable — Update Salary by 10%
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float salary = 50000;
+
+    float &newSalary = salary;
+
+    newSalary = newSalary + (newSalary * 10 / 100);
+
+    cout << "Salary = " << salary << endl;
+
+    return 0;
+}
